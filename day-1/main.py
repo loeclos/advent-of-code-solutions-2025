f920b916b29015753a6c44cf41aac713c7acae76
_v3 <developer.zhukov@gmail.com>
2025-12-03 09:24:03 -0800

    Finish day 1

--- day-1/main.py
+++ day-1/main.py
@@ -2,26 +2,61 @@
 current_position = 50
 zero_counts = 0
 
+# NOTE: PART ONE
+
+# with open('puzzle_input.txt') as f:
+#     for rotation in f.readlines():
+#         if current_position == 0:
+#             zero_counts += 1
+
+#         if rotation.startswith('R'):
+#             rotation_value_r = int(rotation[1:])
+
+#             for r in range(rotation_value_r):
+#                 if current_position == 99:
+#                     current_position = 0
+#                 else:
+#                     current_position += 1
+            
+#         if rotation.startswith('L'):
+#             rotation_value_l = int(rotation[1:])
+            
+#             for r in range(rotation_value_l):
+#                 if current_position == 0:
+#                     current_position = 99
+#                 else:
+#                     current_position -= 1
+            
+# NOTE: PART TWO
+
 with open('puzzle_input.txt') as f:
     for rotation in f.readlines():
+        # if current_position == 0:
+        #     zero_counts += 1
+
         if rotation.startswith('R'):
-            current_sum = current_position + int(rotation[1:])
-            if current_sum > 99:
-                between_sum = current_sum
-                while between_sum > 99:
-                    between_sum -= 99
-                # current_position = (current_sum - 99)
-            else:
-                current_position = current_sum
-        elif rotation.startswith('L'):
-            current_diff = current_position - int(rotation[1:]) 
-            if current_diff < 0:
-                between_sum = current_diff
-                while between_sum < 0:
-                    between_sum += 99
-            else:
-                current_position = current_diff
-        if current_position == 0:
-            zero_counts += 1
-        # print(current_position)
-        print(zero_counts)
+            rotation_value_r = int(rotation[1:])
+
+            for r in range(rotation_value_r):
+                if current_position == 0:
+                    zero_counts += 1
+
+                if current_position == 99:
+                    current_position = 0
+                else:
+                    current_position += 1
+            
+        if rotation.startswith('L'):
+            rotation_value_l = int(rotation[1:])
+            
+            for r in range(rotation_value_l):
+                if current_position == 0:
+                    zero_counts += 1
+
+                if current_position == 0:
+                    current_position = 99
+                else:
+                    current_position -= 1
+
+
+print(zero_counts)
